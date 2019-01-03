66854e42029982ce3181a679c97a6413d5d0ee07
Molly McCarron <mccarronmollye@gmail.com>
2019-01-03 18:20:54 +0000

    first commit

---
+++ lib/codenewbie/cli.rb
@@ -0,0 +1,8 @@
+require "codenewbie/cli/version"
+
+module Codenewbie
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/codenewbie/cli/version.rb
@@ -0,0 +1,5 @@
+module Codenewbie
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
