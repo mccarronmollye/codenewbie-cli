ab36d87497bb77a1f3724bdddbcd932e2444656c
Molly McCarron <mccarronmollye@gmail.com>
2019-01-03 19:35:30 +0000

    Added spec folder

---
+++ spec/codenewbie_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Codenewbie do
+  it "has a version number" do
+    expect(Codenewbie::VERSION).not_to be nil
+  end
+
+#  it "does something useful" do
+#    expect(false).to eq(true)
+#  end
+end
