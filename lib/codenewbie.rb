f6fb93986b2a907b786b46c7a229416faff1e835
Molly McCarron <mccarronmollye@gmail.com>
2019-01-03 19:28:04 +0000

    added codenewbie.rb - requirements

---
+++ lib/codenewbie.rb
@@ -0,0 +1,7 @@
+
+require "pry" #loadpath
+require "nokogiri"
+require "open-uri"
+require_relative 'codenewbie/version' #relative to the file directory
+require_relative 'codenewbie/cli'
+require_relative 'codenewbie/episode'
